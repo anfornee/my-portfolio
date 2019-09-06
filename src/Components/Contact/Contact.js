bcb3bce0d04e915bc3d22f5ad9273da2203b8fb5
Anthony Pereira <anthonyp@ichop.org>
2019-09-05 22:49:50 -0700

    Contact form update

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -4,10 +4,11 @@ const Contact = () => (
     <div className="contact" >
         <div className="contact-container">
             <h1 id="contact" className="contact-title">Contact</h1>
-            <form className="contact-form">
-                <input type="text" placeholder="name"></input>
-                <input type="text" placeholder="email"></input>
-                <textarea type="text" rows="7" placeholder="message"></textarea>
+            <form className="contact-form" name="contact" method="POST" data-netlify="true">
+                <input type="text" name="name" placeholder="name"></input>
+                <input type="text" name="email" placeholder="email"></input>
+                <textarea type="text" name="message" rows="7" placeholder="message"></textarea>
+                <button type="submit" className="contact-btn">Send</button>
             </form>
         </div>
     </div>
