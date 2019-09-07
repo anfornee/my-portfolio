8a45fff9cb85d48c967d84c7175d29584319f040
Anthony Pereira <anthonyp@ichop.org>
2019-09-06 19:14:38 -0700

    Ready for the world. For now...

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -24,7 +24,7 @@ const AboutMe = () => (
                                 I am a design and experience focused Full Stack Developer specializing in Javascript frameworks
                                 including React and AngularJS. With an effective and eloquent front end I believe you
                                 can tell the story of a brand and introduce yourself in a personal way, connecting with the user or
-                                customer without ever meeting face to face. Partner that with a secure and efficient back end and
+                                customer without ever meeting face to face. Partner that with a quality back end and
                                 easily make a lasting impression.
                             </p>
                         </div>
--- src/Components/AboutMe/philolist.js
+++ src/Components/AboutMe/philolist.js
@@ -6,16 +6,16 @@ export const philos = [
     {
         "id": 1,
         "icon": computer,
-        "description": "some really cool idea."
+        "description": "A front end experience should be fluent, intuitive, and consistent. Everything should have a purpose and that purpose needs to be clearly understood. "
     },
     {
         "id": 2,
         "icon": cogs,
-        "description": "some really cool idea."
+        "description": "Users don't usually know how everything works behind the scenes but they'll always know when it doesn't. A beautiful UI is nothing without an efficient and secure backend."
     },
     {
         "id": 3,
         "icon": happy,
-        "description": "some really cool idea."
+        "description": "Happy code leads to happy users! The internet can be the only interaction we have with most people and making sure they have a fantastic experience is the priority."
     }
 ]
