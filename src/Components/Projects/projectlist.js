9dfcada34abc97f4f83e82f2c733d1706db1bca8
Duke-Pereira <ald59@2098adm159316L.local>
2020-02-29 22:01:47 -0800

    Getting those project gifs werkin

--- src/Components/Projects/projectlist.js
+++ src/Components/Projects/projectlist.js
@@ -1,7 +1,10 @@
 import countdown from '../../assets/images/project-images/countdown.png'
+import countdownGif from '../../assets/images/project-images/countdownGif.gif'
 import weather from '../../assets/images/project-images/react-weather.png'
+import weatherGif from '../../assets/images/project-images/weatherGif.gif'
 import todo from '../../assets/images/project-images/angular-todo.png'
-import expressProfile from '../../assets/images/project-images/express-profile.png'
+import soundbridge from '../../assets/images/project-images/soundbridge.png'
+import soundbridgeGif from '../../assets/images/project-images/soundbridgeDemo.gif'
 import responsive from '../../assets/images/project-images/responsive-example.png'
 import lumdb from '../../assets/images/project-images/lumdb.png'
 import testGif from '../../assets/images/project-images/source.gif'
@@ -11,7 +14,7 @@ export const projects = [
     title: 'React Countdown',
     description: 'React front end retrieves events from MongoDB using Express.',
     image: countdown,
-    gif: testGif,
+    gif: countdownGif,
     link: 'https://github.com/anfornee/countdown-react-express-mongodb-app',
     id: 1
   },
@@ -19,7 +22,7 @@ export const projects = [
     title: 'React Weather',
     description: 'React front end using OpenWeatherMap and MapQuest APIs gets current weather for specified location',
     image: weather,
-    gif: testGif,
+    gif: weatherGif,
     link: 'https://github.com/anfornee/react-weather-app',
     id: 2
   },
@@ -32,11 +35,11 @@ export const projects = [
     id: 3
   },
   {
-    title: 'Express Profile',
-    description: 'Create a profile and write/edit post using RESTful system.',
-    image: expressProfile,
-    gif: testGif,
-    link: 'https://github.com/anfornee/express-profile-project',
+    title: 'Soundbridge',
+    description: 'Full MERN stack web app hitting the Spotify API for user data.',
+    image: soundbridge,
+    gif: soundbridgeGif,
+    link: 'https://github.com/bethel-school-of-technology/soundbridge-frontend',
     id: 4
   },
   {
