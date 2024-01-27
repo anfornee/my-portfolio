a9f69259ea5c2326920a9b66577ff3322c2820b8
anfornee <anthonyp@ichop.org>
2024-01-27 10:50:38 -0800

    initial cleanup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.scss'
+
+const App = () => {
+  // const [darkMode, setDarkMode] = useState(window.matchMedia('(prefers-color-scheme: dark)').matches)
+
+  // if (darkMode) document.body.classList.add('dark')
+  // else document.body.classList.remove('dark')
+
+  return (
+    <div className='App'>
+      <h1>hello there...</h1>
+    </div>
+  )
+}
+
+export default App
--- src/Components/Gradients/BlueBack.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const BlueBack = () => (
-  <div id='blue-back' className='blue-back-outer'>
-    <div className='blue-back-inner' />
-  </div>
-)
-
-export default BlueBack
